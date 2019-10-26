d777c4bdae3ed99735ba463eb862b4c2ca46830b
Kelvin <allecan@52gmail.com>
2019-10-26 16:09:44 -0600

    subiendo prueba de clase para guardar datos

--- src/aplicaciónmp3/GuardarDatos.java
+++ src/aplicaciónmp3/GuardarDatos.java
@@ -5,10 +5,87 @@
  */
 package aplicaciónmp3;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
  *
  * @author Kelvin Cano
  */
 public class GuardarDatos {
-    
+
+    private String nombreDeArchivo;
+    private Lista listadoRegistros;
+    private Lista indiceRegistros;
+
+    public GuardarDatos(String nombreDeArchivo) {
+        this.nombreDeArchivo = nombreDeArchivo;
+        this.listadoRegistros = new Lista();
+        this.indiceRegistros = new Lista();
+    }
+
+    public void agregarMP3(Byte id, String nombre_cancion, String nombre_artista, String nombre_album, String fecha_album, String genero, String duracion, String URL, String direccion_pista) {
+        listadoRegistros.insertar(id, nombre_cancion, nombre_artista, nombre_album, fecha_album, genero, duracion, URL, direccion_pista);
+    }
+
+    public void Guardar() throws FileNotFoundException, IOException {
+        RandomAccessFile registro = new RandomAccessFile(nombreDeArchivo, "rw");
+        short punteroAIndice = 0;
+        registro.writeShort(punteroAIndice);
+
+        int contadorIndice = 0;
+        for (int i = 1; i <= listadoRegistros.getSize(); i++) {//recorre la lista
+            MP3 mp3 = listadoRegistros.getInicio();
+            //indiceRegistros.get(contadorIndice).setPos((short) registro.getFilePointer());//getFilePointer obtiene la posicion en la que se va escribiendo                
+
+            registro.write(mp3.getId());
+
+            registro.write(mp3.getNombre_cancion().length());
+            registro.writeChars(mp3.getNombre_cancion());
+
+            registro.write(mp3.getNombre_artista().length());
+            registro.writeChars(mp3.getNombre_artista());
+
+            registro.write(mp3.getNombre_album().length());
+            registro.writeChars(mp3.getNombre_album());
+
+            registro.write(mp3.getGenero().length());
+            registro.writeChars(mp3.getGenero());
+
+            registro.write(mp3.getDuracion().length());
+            registro.writeChars(mp3.getDuracion());
+
+            registro.write(mp3.getURL().length());
+            registro.writeChars(mp3.getURL());
+
+            registro.write(mp3.getDireccion_pista().length());
+            registro.writeChars(mp3.getDireccion_pista());
+        }
+        registro.close();
+    }
+
+    public String getNombreDeArchivo() {
+        return nombreDeArchivo;
+    }
+
+    public void setNombreDeArchivo(String nombreDeArchivo) {
+        this.nombreDeArchivo = nombreDeArchivo;
+    }
+
+    public Lista getListadoRegistros() {
+        return listadoRegistros;
+    }
+
+    public void setListadoRegistros(Lista listadoRegistros) {
+        this.listadoRegistros = listadoRegistros;
+    }
+
+    public Lista getIndiceRegistros() {
+        return indiceRegistros;
+    }
+
+    public void setIndiceRegistros(Lista indiceRegistros) {
+        this.indiceRegistros = indiceRegistros;
+    }
 }
--- src/aplicaciónmp3/Lista.java
+++ src/aplicaciónmp3/Lista.java
@@ -10,30 +10,31 @@ package aplicaciónmp3;
  * @author Kelvin Cano
  */
 public class Lista {
+
     private MP3 inicio;
     private int size;
-         
+
     public Lista() {
         this.inicio = null;
         this.size = 0;
     }
-    
-    public void insertar(Byte id, String nc, String na, String nal, String f, String g, String d, String URL, String dp){
+
+    public void insertar(Byte id, String nc, String na, String nal, String f, String g, String d, String URL, String dp) {
         inicio = agregar(inicio, id, nc, na, nal, f, g, d, URL, dp);
     }
-    
-    private MP3 agregar(MP3 new_canc, Byte id, String nombre_cancion, String nombre_artista, String nombre_album, String fecha_album, String genero, String duracion, String URL, String direccion_pista){
-        if(new_canc==null){
-            new_canc = new MP3(id, nombre_cancion, nombre_artista, nombre_album, fecha_album, genero, duracion, URL, direccion_pista);            
+
+    private MP3 agregar(MP3 new_canc, Byte id, String nombre_cancion, String nombre_artista, String nombre_album, String fecha_album, String genero, String duracion, String URL, String direccion_pista) {
+        if (new_canc == null) {
+            new_canc = new MP3(id, nombre_cancion, nombre_artista, nombre_album, fecha_album, genero, duracion, URL, direccion_pista);
+        } else {
+            if (new_canc.getId() < id) {
+                new_canc.setSig(agregar(new_canc.getSig(), id, nombre_cancion, nombre_artista, nombre_album, fecha_album, genero, duracion, URL, direccion_pista));
+            }
         }
-        else{
-            if(new_canc.getId()<id)
-                new_canc.setSig(agregar(new_canc.getSig(), id, nombre_cancion, nombre_artista, nombre_album, fecha_album, genero, duracion, URL, direccion_pista));            
-        } 
-        size ++;
+        size++;
         return new_canc;
     }
-    
+
 //    public void mostrar(){
 //        Elemento aux = inicio;
 //        if (inicio != null){
@@ -47,8 +48,7 @@ public class Lista {
 //        }
 //        else{System.out.println("No hay elementos para mostrar");}
 //    }
-    
-    public void mostrar(){
+    public void mostrar() {
         MP3 cancion = this.inicio.getSig();
         System.out.println("ID: " + cancion.getId());
         System.out.println("ID: " + cancion.getNombre_cancion());
@@ -59,5 +59,13 @@ public class Lista {
         System.out.println("ID: " + cancion.getDuracion());
         System.out.println("ID: " + cancion.getURL());
         System.out.println("ID: " + cancion.getDireccion_pista());
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public MP3 getInicio() {
+        return inicio;
     }
 }
