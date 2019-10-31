6f829ad119a44555b754d87b4a551efda157652c
Fernando <ferrafterminador@gmail.com>
2019-10-30 20:29:43 -0600

    Agregando boton de añadir carpeta

--- src/aplicaciónmp3/Main.java
+++ src/aplicaciónmp3/Main.java
@@ -12,7 +12,10 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 
@@ -21,48 +24,51 @@ import javax.swing.table.DefaultTableModel;
  * @author ferna
  */
 public class Main extends javax.swing.JFrame {
+
     DefaultTableModel modelo;
+
     /**
      * Creates new form Main
      */
     public Main() throws FileNotFoundException, IOException {
         initComponents();
+        jTable1.setDefaultEditor(Object.class, null);
         setResizable(false);
         setLocationRelativeTo(null);
         LectorArchivos Datos = new LectorArchivos();
-        Imagen fondo = new Imagen("/Imagenes/images.jpg", jPanel1.getWidth(), jPanel1.getHeight()+10);
+        Imagen fondo = new Imagen("/Imagenes/images.jpg", jPanel1.getWidth(), jPanel1.getHeight() + 10);
         jPanel1.add(fondo);
         jPanel1.repaint();
-        if(Datos.VerificarDatos() == false){
+        if (Datos.VerificarDatos() == false) {
             this.AgregarCarpetaMusica();
         }
         this.getData();
     }
 
-    public void getData() throws FileNotFoundException, IOException{
+    public void getData() throws FileNotFoundException, IOException {
         jTable1.getTableHeader().setFont(new Font("Arial", 1, 13));
         // cambia el fondo del encabezado de la tabla
         jTable1.getTableHeader().setBackground(Color.WHITE);
         // cambia el color de la letra del encabezado de la tabla
         jTable1.getTableHeader().setForeground(Color.BLACK);
-        modelo= new DefaultTableModel();
+        modelo = new DefaultTableModel();
         modelo.addColumn("Cancion");
         modelo.addColumn("Artista");
         modelo.addColumn("Album");
         RandomAccessFile lectura = new RandomAccessFile("canciones.data", "rw");
-            short punteroIndice = lectura.readShort();
-            lectura.seek(punteroIndice);
-            for(int i = 0; i < lectura.length(); i++){//Recorre el archivo para obtener los indices
-                short posicionDatos = lectura.readShort();
-                byte longitud = lectura.readByte();
-                byte[] caracteres = new byte[longitud*2];
-                lectura.read(caracteres);
-                String cancion = new String(caracteres);
-                modelo.addRow(new Object[]{cancion,this.getArtista(posicionDatos), this.getAlbum(posicionDatos)});
-                if(lectura.getFilePointer() == lectura.length()){//Rompe si llega al final del archivo
-                    break;
-                }
+        short punteroIndice = lectura.readShort();
+        lectura.seek(punteroIndice);
+        for (int i = 0; i < lectura.length(); i++) {//Recorre el archivo para obtener los indices
+            short posicionDatos = lectura.readShort();
+            byte longitud = lectura.readByte();
+            byte[] caracteres = new byte[longitud * 2];
+            lectura.read(caracteres);
+            String cancion = new String(caracteres);
+            modelo.addRow(new Object[]{cancion, this.getArtista(posicionDatos), this.getAlbum(posicionDatos)});
+            if (lectura.getFilePointer() == lectura.length()) {//Rompe si llega al final del archivo
+                break;
             }
+        }
 
 //        for (int i = 0; i < tabla.getListaArreglo(); i++) {
 //            }
@@ -80,39 +86,39 @@ public class Main extends javax.swing.JFrame {
 //        ColorFila color2=new ColorFila(2);
 //        tablita.getColumnModel().getColumn(2).setCellRenderer(color2);
     }
-    
-    public String getArtista(int posicion) throws IOException{
+
+    public String getArtista(int posicion) throws IOException {
         RandomAccessFile lectura = new RandomAccessFile("canciones.data", "rw");
         lectura.seek(posicion);
         byte longitud = lectura.readByte();
-        byte[] caracteres = new byte[longitud*2];
+        byte[] caracteres = new byte[longitud * 2];
         lectura.read(caracteres);
         String cancion = new String(caracteres);
         return cancion;
     }
-    
-    public String getAlbum(int posicion) throws IOException{
+
+    public String getAlbum(int posicion) throws IOException {
         RandomAccessFile lectura = new RandomAccessFile("canciones.data", "rw");
         lectura.seek(posicion);
         byte longitud = lectura.readByte();
-        byte[] caracteres = new byte[longitud*2];
+        byte[] caracteres = new byte[longitud * 2];
         lectura.read(caracteres);
         String cancion = new String(caracteres);
         byte longitud1 = lectura.readByte();
-        byte[] caracteres1 = new byte[longitud1*2];
+        byte[] caracteres1 = new byte[longitud1 * 2];
         lectura.read(caracteres1);
         String cancion1 = new String(caracteres1);
         return cancion1;
     }
-    
-    public void AgregarCarpetaMusica() throws IOException{
+
+    public void AgregarCarpetaMusica() throws IOException {
         JFileChooser jf = new JFileChooser();
-        jf.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); 
-        jf.showSaveDialog(null); 
+        jf.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        jf.showSaveDialog(null);
         LectorArchivos lee = new LectorArchivos(jf.getSelectedFile().getAbsolutePath());
         lee.SaveInformation();
     }
-    
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -122,13 +128,16 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jToggleButton1 = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -141,53 +150,96 @@ public class Main extends javax.swing.JFrame {
 
             }
         ));
+        jTable1.setEditingColumn(1);
+        jTable1.setEditingRow(1);
+        jTable1.setSelectionBackground(new java.awt.Color(51, 51, 255));
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
+
+        jLabel1.setFont(new java.awt.Font("Yu Gothic", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(102, 255, 0));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("MI LISTA DE CANCIONES");
+
+        jToggleButton1.setBackground(new java.awt.Color(153, 255, 0));
+        jToggleButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/AgregarMusica.jpg"))); // NOI18N
+        jToggleButton1.setText("<html>Abrir nueva carpeta de música</html>");
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(19, 19, 19)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 921, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(25, Short.MAX_VALUE))
+                .addGap(23, 23, 23)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 913, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(34, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(301, 301, 301))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jToggleButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(49, 49, 49))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(220, Short.MAX_VALUE))
+                .addGap(22, 22, 22)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 372, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(34, 34, 34)
+                .addComponent(jToggleButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(94, Short.MAX_VALUE))
         );
-
-        jTabbedPane1.addTab("Mi música", jPanel1);
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 965, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 633, Short.MAX_VALUE)
-        );
-
-        jTabbedPane1.addTab("Buscar", jPanel2);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jTabbedPane1)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jTabbedPane1)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+        try {
+            this.AgregarCarpetaMusica();
+            this.getData();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
+
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+        JTable source = (JTable) evt.getSource();
+        //            //int row = source.rowAtPoint( evt.getPoint() );
+        //            int column = source.columnAtPoint( evt.getPoint() );
+        //            String s=source.getModel().getValueAt(column, 1)+"";
+        int index = jTable1.getSelectedRow();
+        String s = source.getModel().getValueAt(index, 0) + "  " + source.getModel().getValueAt(index, 1);//Jala el primer elemento seleccionado
+        //JOptionPane.showMessageDialog(null, s);
+        final ImageIcon icon = new ImageIcon("src/Imagenes/D.png");
+        //JOptionPane.showMessageDialog(null, s, "Información acerca de la canción", JOptionPane.INFORMATION_MESSAGE, icon);
+    }//GEN-LAST:event_jTable1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -229,14 +281,13 @@ public class Main extends javax.swing.JFrame {
             }
         });
     }
-    
 
-    
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JToggleButton jToggleButton1;
     // End of variables declaration//GEN-END:variables
 }
